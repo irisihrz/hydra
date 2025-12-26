05ccccb52916d440b40e7bfdc430b3d26322d0b7
camilledaguin <140817611+irisihrz@users.noreply.github.com>
2025-12-26 18:31:22 +0100

    macos compatibility

--- python_rpc/setup.py
+++ python_rpc/setup.py
@@ -1,11 +1,15 @@
 from cx_Freeze import setup, Executable
+import sys
 
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {
     "packages": ["libtorrent"],
     "build_exe": "hydra-python-rpc",
-    "include_msvcr": True
 }
+
+# Windows-specific options
+if sys.platform == "win32":
+    build_exe_options["include_msvcr"] = True
 
 setup(
     name="hydra-python-rpc",
@@ -13,8 +17,8 @@ setup(
     description="Hydra",
     options={"build_exe": build_exe_options},
     executables=[Executable(
-      "python_rpc/main.py",
+      "main.py",
       target_name="hydra-python-rpc",
-      icon="build/icon.ico"
+      icon="build/icon.ico" if sys.platform == "win32" else None
     )]
 )
